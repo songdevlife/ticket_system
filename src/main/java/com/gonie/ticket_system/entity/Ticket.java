b002224bc27b2665c48d7199aa3d6dcfc99d8b5e
Gonie Song <gonie@Gonies-Air.gateway>
2026-05-14 01:05:12 +0930

    feat: add Ticket entity with JPA annotations

---
+++ src/main/java/com/gonie/ticket_system/entity/Ticket.java
@@ -0,0 +1,14 @@
+package com.gonie.ticket_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ticket {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
